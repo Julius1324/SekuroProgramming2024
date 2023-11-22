70df5afb54d3c53109202f6e64c82a89a59fefe0
Arthur <juliusarthur2005@gmail.com>
2023-11-22 17:42:45 +0700

    Fungsi dan while loop

--- Postday2.cpp
+++ Postday2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <math.h>
 using namespace std;
 void lokasi(float x,float y){
     std::cout << std::setprecision(2) << std::fixed;
@@ -9,21 +10,41 @@ void gerak(float x2,float y2,float* x,float* y){
     *x += x2;
     *y += y2;
 }
+void gerak_2(float v, float t, float theta, float* x,float* y){
+    float pi = 3.14159265359;
+    *x += v*sin((theta*pi)/180)*t;
+    *y += v*cos((theta*pi)/180)*t;
+
+}
+
 
 int main(){
     bool game_on=true;
     float x=0;float y=0;
+    cout<<"lokasi: lokasi()"<<endl;
+    cout<<"gerak: geraj(x,y)"<<endl;
+    cout<<"gerak_2: gerak_2(v,t,theta)"<<endl;
+    cout<<"exit: keluar dari program"<<endl;
+    cout<<"Masukkan perintah"<<endl;
     while (game_on==true){
-        string input;
-        cin>>input;
-        lokasi(x,y);
-        float x1,x2,xpointer,ypointer;
-        cin>>x1>>x2;
-
-        gerak(x1,x2,&x,&y);
-        lokasi(x,y);
-        cout<<"selesai"<<endl;
-    }
-    
-
+        string input;cin>>input;
+        if (input=="lokasi"){
+            lokasi(x,y);
+        }
+        if (input=="gerak"){
+            float x2;float y2;
+            cout<<"x y"<<endl;
+            cin>>x2>>y2;
+            gerak(x2,y2,&x,&y);
+        }
+        if (input=="gerak_2"){
+            float v;float t;float theta;
+            cout<<"v t theta"<<endl;
+            cin>>v>>t>>theta;
+            gerak_2(v,t,theta,&x,&y);
+        }
+        if (input=="exit"){
+            break;
+        }
+        }
 }
