25c2c36bb356456b7e76388a90ee83b8a741087b
Arthur <juliusarthur2005@gmail.com>
2023-11-26 11:18:10 +0700

    tes

---
+++ ngetes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> v1={1,2,3,4,5,6,7,8};
+    v1.push_back(12);
+    v1.resize(8);
+    for (auto i=v1.begin();i != v1.end();i++){
+        cout<<*i;
+    }
+}
+
+// 1 2 3 
+//index 4
