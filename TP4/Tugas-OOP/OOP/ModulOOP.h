a2f5bcbb9d5f9fa6c4f122b043aeeef8e23c8c51
Arthur <juliusarthur2005@gmail.com>
2023-12-31 20:56:11 +0700

    all

---
+++ TP4/Tugas-OOP/OOP/ModulOOP.h
@@ -0,0 +1,37 @@
+#ifndef ModulOOP_H
+#define ModulOOP_H
+#include <vector>
+using namespace std;
+
+class Matrix{
+    private:
+        int baris;
+        int kolom;
+    public:
+        vector<vector<float>> v;
+        Matrix();
+        Matrix(vector<vector<float>> vin);
+        Matrix(int b, int k);
+        void display();
+        void get_baris();
+        void get_kolom();
+        Matrix operator+ (Matrix a);
+        Matrix operator- (Matrix a);
+        Matrix operator* (Matrix a);
+        };
+
+class Swerve{
+    private:
+    Matrix pers1_mat;
+    public:
+    float v1x,v1y,v2x,v2y,v3x,v3y,v4x,v4y;
+    float v1,v2,v3,v4;
+    float vx, vy, omega;
+    float x,y,teta;
+
+    Swerve(vector<vector<float>> vin);
+    void velocityCommands(float v_x, float v_y, float w1);
+    void updatePose(float deltaTime);
+};
+
+#endif
